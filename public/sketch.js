a7b882617eb0c92c1f033359570d571e1b40d7c9
BuildTools <unconfigured@null.spigotmc.org>
2021-01-21 22:37:24 +0300

    canvas

--- public/sketch.js
+++ public/sketch.js
@@ -18,15 +18,29 @@ let RGB = [
 let coords;
 let dia = 50;
 let socket;
-let colorfulMode = false;
+let colorfulMode = 0;
+let canvas;
 function setup() {
-	createCanvas(windowWidth, windowHeight);
+	 canvas = createCanvas(windowWidth, windowHeight);
 	background(50, 255, 50);
 	
 socket = io();
 
-	socket.on('draw', newDrawing);
+	socket.on('canvas', canvas => {
+		loadPixels();
+		console.log(pixels);
+		console.log(canvas)
+		for(let i = 0; i > pixels.length; i+=3){
+			pixels[i] = canvas[i];
+			pixels[i+1] = canvas[i+1]
+			pixels[i+2] = canvas[i+2]
+		}
+		updatePixels();
+		console.log("recieved");
+	})
+//	socket.on('draw', newDrawing);
 	socket.on('draw3', newDrawing3);
+	socket.on('drawLine', newDrawingLine)
 }
 function newDrawing3(draw3Data){
 	for(let i = 0; i < 5; ++i){
@@ -38,8 +52,14 @@ function newDrawing3(draw3Data){
 }
 function newDrawing(drawData){
 	noStroke();
-	fill(drawData.RGB[0].color[0], drawData.RGB[0].color[1], drawData.RGB[0].color[2]);
+	fill(drawData.rgb[0].color[0], drawData.rgb[0].color[1], drawData.rgb[0].color[2]);
 	ellipse(drawData.X, drawData.Y, drawData.dia);
+}
+function newDrawingLine(drawLineData){
+	strokeWeight(drawLineData.dia);
+	stroke(drawLineData.rgb.color[0], drawLineData.rgb.color[1], drawLineData.rgb.color[2]);
+	line(drawLineData.X, drawLineData.Y, drawLineData.pX, drawLineData.pY)
+
 }
 function draw() {
 
@@ -50,31 +70,46 @@ sendingDrawEmits()
 	}
 }
 function sendingDrawEmits(){
-if(!colorfulMode) {
-	fill(RGB[0].color[0], RGB[0].color[1], RGB[0].color[2]);
-	ellipse(mouseX, mouseY, dia, dia);
-	noStroke();
-	drawData = {
+// if(colorfulMode === 0) {
+// 	fill(RGB[0].color[0], RGB[0].color[1], RGB[0].color[2]);
+// 	ellipse(mouseX, mouseY, dia, dia);
+// 	noStroke();
+// 	drawData = {
+// 		X: mouseX,
+// 		Y: mouseY,
+// 		rgb: RGB,
+// 		dia: dia
+// 	}
+// 	//console.log(drawData);
+// 	socket.emit('draw', drawData);
+// } else
+	if(colorfulMode === 0){
+	strokeWeight(dia);
+	stroke(RGB[0].color[0], RGB[0].color[1], RGB[0].color[2]);
+	line(mouseX, mouseY, pmouseX, pmouseY)
+	drawLineData= {
 		X: mouseX,
 		Y: mouseY,
-		RGB: RGB,
+		pX: pmouseX,
+		pY: pmouseY,
+		rgb: RGB[0],
 		dia: dia
 	}
-	//console.log(drawData);
-	socket.emit('draw', drawData);
-} else {
-	for(let i = 0; i < 5; ++i){
-		resetCoords();
-		noStroke();
-		fill(RGB[i].color[0], RGB[i].color[1], RGB[i].color[2])
-		ellipse(coords[i+1].xy[0], coords[i+1].xy[1], coords[0].dia2, coords[0].dia2);
-	}
-	draw3Data = {
-		coords : coords,
-		rgb : RGB
-	}
-	socket.emit('draw3', draw3Data);
+socket.emit('drawLine', drawLineData)
 }
+	else if(colorfulMode === 1){
+		for(let i = 0; i < 5; ++i){
+			resetCoords();
+			noStroke();
+			fill(RGB[i].color[0], RGB[i].color[1], RGB[i].color[2])
+			ellipse(coords[i+1].xy[0], coords[i+1].xy[1], coords[0].dia2, coords[0].dia2);
+		}
+		draw3Data = {
+			coords : coords,
+			rgb : RGB
+		}
+		socket.emit('draw3', draw3Data);
+	}
 }
 function mouseWheel(event){
 
@@ -91,7 +126,11 @@ function mousePressed(){
 }
 function keyTyped(){
 	if(key.toLowerCase() === "3"){
-		colorfulMode = !colorfulMode;
+		if(colorfulMode === 1){
+			colorfulMode = 0;
+		} else {
+			colorfulMode++
+		}
 	} else {
 		switch (key.toLowerCase()) {
 			case "w":
@@ -118,6 +157,13 @@ function keyTyped(){
 			case "g":
 				changeRGB([0, 200, 20]);
 				break;
+			// case "t":
+			// loadPixels();
+			// const data = pixels;
+			// console.log(data)
+			// socket.emit('canvas', data);
+			// console.log(pixelDensity());
+			// 	break;
 		}
 	}
 
@@ -151,3 +197,29 @@ function resetCoords(){
 		}
 	]
 }
+// function compressArray(array){
+// 	let arrayy = array;
+//    let Array = []
+// 	let num = 0;
+//    let constant = []
+//
+// 		for(let n = 0; n < array.length; n++){
+// if(arrayy.length < 4){
+// 	break;
+// }
+// 			if(constant.length < 1){
+// 				constant.push(arrayy[0], arrayy[1], arrayy[2], arrayy[3])
+// 			} else if(constant[0] === arrayy[0] || constant[1] === arrayy[1] || constant[2] === arrayy[2] || constant[3] === arrayy[3]){
+// 				num++
+// 				arrayy.slice(0, 4);
+// 				console.log("a")
+// 			} else if(constant[0] !== arrayy[0] || constant[1] !== arrayy[1] || constant[2] !== arrayy[2] || constant[3] !== arrayy[3]){
+//
+// 				Array.push([arrayy[0], arrayy[1], arrayy[2], arrayy[3], num])
+// 				num = 0;
+// 				constant = [arrayy[0], arrayy[1], arrayy[2], arrayy[3]]
+// 			}
+// 		}
+// return Array;
+//
+// }
--- server.js
+++ server.js
@@ -3,11 +3,12 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 const server = app.listen(port);
+const fs = require('fs');
 let firstClient;
 
-app.use(express.static('public'));
+    app.use(express.static('public'));
 
-
+fs.writeFile("emits.txt", "", err => {})
 console.log("My socket server is running!");
 const socket = require('socket.io');
 const io = socket(server);
@@ -15,27 +16,55 @@ const io = socket(server);
 io.sockets.on('connection', newConnection);
 // function for the event
 function newConnection(socket){
-    
-if(firstClient === undefined){
-    firstClient = socket.client.id;
-} else {
-
-}
+    console.log(socket.id)
+    const emits = fs.readFileSync("emits.txt", "utf8").split("\n");
+    if(emits[0] !== '') {
+        for (let i = 0; i < emits.length; ++i) {
+            io.to(socket.id).emit(JSON.parse(emits[i])["emit"], JSON.parse(emits[i])["data"]);
+        }
+    }
   //console.log(socket.client)
-socket.on('draw', drawEvent);
+// socket.on('draw', drawEvent);
 socket.on('draw3', draw3Event);
+socket.on('drawLine', drawLineEvent)
+    function drawLineEvent(drawLineData){
+    if(drawLineData.dia > 250){
+        drawLineData.dia = 250;
+    }
+        append("drawLine", drawLineData);
+    socket.broadcast.emit('drawLine', drawLineData)
+    }
 function draw3Event(draw3Data){
 if(draw3Data.coords[0].dia2 > 75){
    draw3Data.coords[0].dia2 = 75;
 }
+      append("draw3",draw3Data);
+
         socket.broadcast.emit('draw3', draw3Data)
 }
-function drawEvent(drawData){
-    if(drawData.dia > 250){
-        drawData.dia = 250;
-    }
-    socket.broadcast.emit('draw', drawData);
-}
+// function drawEvent(drawData){
+//     if(drawData.dia > 250){
+//         drawData.dia = 250;
+//     }
+//     socket.broadcast.emit('draw', drawData);
+// }
 
 }
-module.exports = {port};
+function append(emit,data) {
+    const limit = fs.readFileSync("emits.txt", "utf8").split("\n");
+    if (limit.length >= 300) {
+        limit.shift();
+        fs.writeFileSync("emits.txt", limit.toString().replace(/},{"e/g, "}\n{\"e"), err => {
+            if (err !== null) {
+                console.log(err)
+            }
+        });
+
+    }
+    if (fs.readFileSync("emits.txt", "utf8") === "") {
+        fs.appendFileSync("emits.txt", JSON.stringify({emit: emit, data: data}))
+
+    } else{
+        fs.appendFileSync("emits.txt", "\n" + JSON.stringify({emit: emit, data: data}))
+}
+}
